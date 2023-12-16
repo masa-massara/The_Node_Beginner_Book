e7ab161b340feaa84d87f9d8acd3d13e69e7a62e
masa-massara <masaya.yoshikawa.31@icloud.com>
2023-12-16 19:38:13 +0900

    イベント駆動でコールバック関数が動いていることの確認

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require("http");
 
-const onRequest = (request, response) => {
+function onRequest(request, response) {
+  console.log("Request received.");
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
@@ -8,3 +9,4 @@ const onRequest = (request, response) => {
 
 http.createServer(onRequest).listen(8888);
 
+console.log("Server has started.");

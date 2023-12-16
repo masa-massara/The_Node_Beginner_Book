6bb48515b43c852700ae6639a182c66e3f9fe8d5
masa-massara <masaya.yoshikawa.31@icloud.com>
2023-12-16 19:10:32 +0900

    createServerをアロー関数で書いた

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var http = require("http");
 
-http
-  .createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Hello World");
-    response.end();
-  })
-  .listen(8888);
+const onRequest = (request, response) => {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}
+
+http.createServer(onRequest).listen(8888);
+
